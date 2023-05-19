861714010b1b23922e19fe74ab6ad17b762539cc
didrik <didrik.sommersten@gmail.com>
2023-05-19 21:33:32 +0200

    jeg vil bare ikke mer

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const chance = document.getElementById("chance").value;
-const perms = document.getElementById("perms").value;
+const chance = document.getElementById("chance");
+const perms = document.getElementById("perms");
 const answer = document.getElementById("answer");  
 const submit = document.getElementById("submit");
 
-submit.addEventListener('click', function() {
-  realProb()
-})
-
 function realProb() {
-  if (chance < 0 || chance > 100 || perms < 1) return;
+  if (chance.value < 0 || chance.value > 100 || perms.value < 1) return;
 
-  const frac = (100 - chance) / 100;
-  const calc = Math.pow(frac, perms);
+  const frac = (100 - chance.value) / 100;
+  const calc = Math.pow(frac, perms.value);
   const ans = (1 - calc) * 100;
   
   if (ans < 100) answer.textContent = ans; 
   else if (ans == 100) answer.textContent = ">99.99999999999999"
 }
+
+submit.addEventListener('click', realProb);

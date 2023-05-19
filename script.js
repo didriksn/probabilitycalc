8884daea2a8b5b477a01232ffa57bc2ec9483913
didriksn <102746612+didriksn@users.noreply.github.com>
2023-05-19 20:20:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+const chance = document.getElementById("chance").value;
+const perms = document.getElementById("perms").value;
+const answer = document.getElementById("answer");  
 const submit = document.getElementById("submit");
+
 submit.addEventListener('click', function() {
   realProb()
 })
 
-
 function realProb() {
-  const chance = document.getElementById("chance").value;
-  const perms = document.getElementById("perms").value;
-  const answer = document.getElementById("answer");  
-
   if (chance < 0 || chance > 100 || perms < 1) return;
 
   const frac = (100 - chance) / 100;
